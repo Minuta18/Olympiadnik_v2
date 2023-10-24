99b54f5410dfbb0455d65e93540d8d6bb7bf25fc
Minuta18 <ig.samsonov10@yandex.ru>
2023-10-24 09:01:30 +0300

    Configured first service

---
+++ auth/app/models.py
@@ -0,0 +1,2 @@
+import sqlalchemy as sql
+import app
---
+++ auth/app/views.py
@@ -0,0 +1,14 @@
+from passlib import context
+import fastapi
+import app
+
+router = fastapi.APIRouter(prefix=f'{app.PREFIX}/auth')
+pwd_context = context.CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+async def init_models():
+    async with app.engine.begin() as conn:
+        await conn.run_sync(app.base.metadata.create_all)
+
+async def destroy_models():
+    async with app.engine.begin() as conn:
+        await conn.run_sync(app.base.metadata.drop_all)
